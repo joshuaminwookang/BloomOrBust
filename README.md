8d7bca286aa026c796536da9efa1dabaa6d84fbc
joshuaminwookang <jmk@jmks-MacBook-Air.local>
2019-12-13 22:18:47 -0500

    final wrapup

--- README.md
+++ README.md
@@ -38,3 +38,25 @@ CUDA Implementation: './bloom-cuda file1 file2'
 #SORT: 	      sort arrays before processing
 
 - Edit BLOCK_SIZE to a float to try different block sizes.
+
+## Information on the HW Accelerator Code
+
+- hw_src/bloom.scala is the Chisel code to generate BF accelerator as RoCC co-processor
+- hw_src/hw-bloom-script.c is the test script that uses BF RoCC instructions
+- hw_src/sw-bloom-script.c is the test script that only uses software to compute the same Map() and Test() operations
+- Comment/Uncomment to direct the test scripts to use different data sets:
+
+#TINY  : use 11 word data for both map and test
+#TINYV2  :  use 30 word data for both map and test
+#TINYV3_Map  : use 50 word data for map and 30 word for test
+#TINYV3_Test : use 50 word data for map and 50 word for test
+
+- *_data.h are header fills containing hard-coded word arrays (we need this because the RISC-V gcc does not support file or stream-based library functions, nor dynamic memory allocation)
+
+The Bloom filter Chisel file is meant to be placed in the Rocket-chip directory: 
+https://github.com/freechipsproject/rocket-chip
+and as in our case, it is a good idea to use Berkeley's Chipyard framework: 
+https://github.com/ucb-bar/chipyard
+
+To generate RISC-V binary workloads from our test scripts, please refer to the FireMarshal Documentation:
+https://github.com/firesim/FireMarshal
--- hw_src/bloom.scala
+++ hw_src/bloom.scala
@@ -1,4 +1,8 @@
-//see LICENSE for license
+// RoCC Bloom filter accelerator
+// (c) 2019 Josh Kang and Andrew Thai
+
+// Current version hard-codes the BF, m = 20,000 and k = 5
+// To-do: parameterize m and k; make accelerator more scalable
 
 package bloom
 
@@ -12,7 +16,8 @@ import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tile._
 import freechips.rocketchip.util.InOrderArbiter
 
-class BloomAccel(opcodes: OpcodeSet, val m: Int = 20000, val k: Int = 5)
+
+class BloomAccel(opcodes: OpcodeSet)
 (implicit p: Parameters) extends LazyRoCC(
     opcodes) {
   override lazy val module = new BloomAccelImp(this)
@@ -20,45 +25,97 @@ class BloomAccel(opcodes: OpcodeSet, val m: Int = 20000, val k: Int = 5)
 
 class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
   // accelerator memory 
-  val bloom_bit_array = Reg(init = Vec.fill(outer.m)(0.U(1.W)))
+  val bloom_bit_array = RegInit(Vec(Seq.fill(20000)(0.U(1.W))))
   val miss_counter = RegInit(0.U(64.W))
-  val busy = Reg(init = Vec.fill(outer.m){Bool(false)})
+  // val busy = RegInit(Bool(false))
 
+  // decode RoCC custom function
   val cmd = Queue(io.cmd)
   val funct = cmd.bits.inst.funct
   val hashed_string = cmd.bits.rs1
-
-  // decode RoCC custom function
   val doInit = funct === UInt(0)
   val doMap = funct === UInt(1)
   val doTest = funct === UInt(2)
 
-  // val wdata = UInt(0)
-  val testMatch = RegInit(Bool(true))
-  val mapModule = Module(new MapBloomModule(outer.m,outer.k))
-  val testModule = Module(new TestBloomModule(outer.m,outer.k)) 
+  // Hash computation hardware units
+  val x0  = Wire(UInt())
+  val y0  = Wire(UInt())
 
+  val x1  = Wire(UInt())
+  val y1  = Wire(UInt())
+
+  val x2  = Wire(UInt())
+  val y2  = Wire(UInt())
+
+  val x3  = Wire(UInt())
+  val y3  = Wire(UInt())
+
+  val x4  = Wire(UInt())
+  val y4  = Wire(UInt())
+
+  val x5  = Wire(UInt())
+  val y5  = Wire(UInt())
+
+  // wiring logic for hashing
+  x0 := hashed_string
+  y0 := hashed_string >> 4
+
+  x1 := (x0 + y0) % 20000.U(64.W)
+  y1 := (y0 + 0.U(64.W)) % 20000.U(64.W)
+
+  x2 := (x1 + y1) % 20000.U(64.W)
+  y2 := (y1 + 1.U(64.W)) % 20000.U(64.W)
+
+  x3 := (x2 + y2) % 20000.U(64.W)
+  y3 := (y2 + 2.U(64.W)) % 20000.U(64.W)
+
+  x4 := (x3 + y3) % 20000.U(64.W)
+  y4 := (y3 + 3.U(64.W)) % 20000.U(64.W)
+
+  x5 := (x4 + y4) % 20000.U(64.W)
+  y5 := (y4 + 4.U(64.W)) % 20000.U(64.W)
+
+  // logic for looking up elements in the bit array
+
+  val found1 = Wire(UInt())
+  val found2 = Wire(UInt())
+  val found3 = Wire(UInt())
+  val found4 = Wire(UInt())
+  val found5 = Wire(UInt())
+
+  found1 := bloom_bit_array(x1)
+  found2 := bloom_bit_array(x2)
+  found3 := bloom_bit_array(x3)
+  found4 := bloom_bit_array(x4)
+  found5 := bloom_bit_array(x5)
+
+  // Custom function behaviors
   when (cmd.fire()) {
     when (doInit) {
-      bloom_bit_array := Reg(init = Vec.fill(outer.m)(0.U(1.W)))
+      // BF_INIT
+      bloom_bit_array := Reg(init = Vec.fill(20000)(0.U(1.W)))
       miss_counter := RegInit(0.U(64.W))
     }
     when (doMap) {
-      mapModule.io.input_value := hashed_string
-      bloom_bit_array := mapModule.io.output_hashBits 
-      printf("I'm mapping !!!! %d\n", hashed_string)
-    }
+      // BF_MAP : map hash indices to array
+      bloom_bit_array(x1) := 1.U(1.W)
+      bloom_bit_array(x2) := 1.U(1.W)
+      bloom_bit_array(x3) := 1.U(1.W)
+      bloom_bit_array(x4) := 1.U(1.W)
+      bloom_bit_array(x5) := 1.U(1.W)
+    } 
     when (doTest) {
-      testModule.io.input_value := hashed_string
-      testModule.io.input_bit_array := bloom_bit_array
-      miss_counter := Mux(testModule.io.output_boolean, miss_counter, miss_counter+1.U(64.W))
-    }
-  }
+      // BF__TEST : hash and check if element was mapped before
+      miss_counter := miss_counter + ~(found1 & found2 & found3 & found4 & found5)
+    } 
+  } 
+
+
 
   // PROCESSOR RESPONSE INTERFACE
-  // Control for communicate accelerator response back to host processor
+  // Control for accelerator to communciate response back to host processor
   val doResp = cmd.bits.inst.xd
-  val stallResp = doResp && !io.resp.ready
+  val stallResp = doResp && !io.resp.ready 
 
   cmd.ready := !stallResp 
     // Command resolved if no stalls AND not issuing a load that will need a request
@@ -67,24 +124,14 @@ class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCM
   io.resp.bits.rd := cmd.bits.inst.rd
     // Write to specified destination register address
   io.resp.bits.data := miss_counter
-    // Send out 
-  io.busy := cmd.valid || busy.reduce(_||_)
+    // Send out miss counter data
+  io.busy := cmd.valid 
     // Be busy when have pending memory requests or committed possibility of pending requests
   io.interrupt := Bool(false)
     // Set this true to trigger an interrupt on the processor (not the case for our current simplified implementation)
 } 
 
-// class OpcodeSet(val opcodes: Seq[UInt]) {
-//   def |(set: OpcodeSet) =
-//     new OpcodeSet(this.opcodes ++ set.opcodes)
-
-//   def matches(oc: UInt) = opcodes.map(_ === oc).reduce(_ || _)
-// }
-
-// object OpcodeSet {
-//   def custom0 = new OpcodeSet(Seq(Bits("b0001011")))
-// }
-
+// With this, the Rocket core will be compiled with BF accelerator as RoCC coprocessor
 class WithBloomAccel extends Config ((site, here, up) => {
 
   case BuildRoCC => Seq(
